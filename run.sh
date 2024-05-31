9a585f46628c639bebe34d2cdfef8bd0b2d0d721
fesinia <165068389+fesiniabediana@users.noreply.github.com>
2024-06-01 06:38:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-30115717-918b-4450-83a0-c9ea00f895cd/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-863dabbe-8762-4fc3-82df-9dbdbaa4f471/)
 
 
 echo "$curl_output" >> Update.tsx
